4c6ea51ba472e09394ef0da4ff90043779f04172
asallh <ajaypalsallh@gmail.com>
2025-02-21 09:29:36 -0700

    electron initiaized

---
+++ src/ui/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="text-3xl font-bold underline text-center">
+        Hello SimpleFTP
+      </div>
+    </div>
+  );
+}
+
+export default App;
